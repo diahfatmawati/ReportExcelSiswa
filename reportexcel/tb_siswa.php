1f503a673fe7853bc71025dc19502a43be3fce13
Diah Fatmawati <61172685+diahfatmawati@users.noreply.github.com>
2020-04-15 08:48:17 +0700

    Report Excel di PHP menggunakan PhpSpreadsheet
    Praktikum Pertemuan 11 Pemrograman Web Report Excel di PHP menggunakan PhpSpreadsheet  
    
    Diah Fatmawati
    18082010012

---
+++ reportexcel/tb_siswa.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db_siswa";
+
+// create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// check connection
+if (!$conn) {
+	die("Connection failed : " .mysqli_connect_error());
+}
+
+$sql = "CREATE TABLE tb_siswa ( 
+		id_siswa INT(11) AUTO_INCREMENT PRIMARY KEY NOT NULL, 
+		nama VARCHAR(225) NOT NULL, 
+		kelas VARCHAR(100) NOT NULL, 
+		alamat VARCHAR(225) NOT NULL)";
+	
+if (mysqli_query($conn, $sql)) {
+	echo "New record created successfully";
+} else {
+	echo "Error : " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
